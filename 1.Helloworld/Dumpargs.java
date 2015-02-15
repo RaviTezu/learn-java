5a54d183440b6e4fa7d5fab7754f81cac7b77727
RaviTeju <ravi-teja@live.com>
2015-02-16 00:06:33 +0530

    Add Dumpargs program

---
+++ 1.Helloworld/Dumpargs.java
@@ -0,0 +1,8 @@
+public class Dumpargs {
+    public static void main(String[] args){
+        System.out.println("Passed arguments are:");
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
